e4a66783f61cd7bedfb1c4259f54e59d2d0b345e
Manoj Naidu <manojnaidu619@gmail.com>
2020-03-15 22:42:44 +0530

    Initial commit

---
+++ brain.rb
@@ -0,0 +1,58 @@
+
+@program_data = ARGF.read.to_s
+@program_space = Array.new(65536,0)   # Dealing with 2^16 data size
+@data_ptr = 0
+
+program_length = @program_data.length
+index = 0
+
+def compute_loop_length(index)
+    end_index = index
+    while true
+        break if @program_data[end_index] == ']'
+        end_index += 1
+    end
+    return end_index
+end
+
+def compute_program(current_char, index)
+    if !current_char.nil?
+        case current_char
+            when "+" then @program_space[@data_ptr] += 1 if @program_space[@data_ptr] >= 0
+            when "-" then @program_space[@data_ptr] -= 1 if @program_space[@data_ptr] > 0
+            when "." then print @program_space[@data_ptr].chr 
+            when ">" then @data_ptr += 1  
+            when "<" then @data_ptr -= 1 
+            when "[" then return compute_loop(index+1)      
+        end 
+    end   
+end
+
+def compute_loop(index)
+    start_index = index
+    end_index = compute_loop_length(index)
+    counter = @program_space[@data_ptr]
+    if counter > 0
+        while index <= end_index
+            current_char = @program_data[index]
+            if current_char == ']'
+                index = start_index-1 if @program_space[@data_ptr] != 0
+            else
+                compute_program(current_char, index)
+            end
+            index += 1
+        end
+    end
+    return end_index+1
+end
+
+while index < program_length
+    current_char = @program_data[index]
+    if current_char == "["
+        index = (compute_program(current_char, index))-1
+    else
+        compute_program(current_char, index)
+    end
+    index += 1
+end
+
